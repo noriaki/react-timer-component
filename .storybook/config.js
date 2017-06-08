62e001d3deb8f643b4d993b4e0627ef6c3776cf7
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-06-08 18:14:49 +0900

    move storybooks directory, path

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure, setAddon } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
 
 const loadStories = () => {
-  require('../src/stories/timer-story'); // eslint-disable-line global-require
+  require('../stories/timer-story'); // eslint-disable-line global-require
 };
 
 setAddon(infoAddon);
